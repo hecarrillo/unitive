883697997f91aa00039fa99f58dffbee8002b691
Megacharles56 <carloscid56@hotmail.com>
2024-09-24 10:03:02 -0600

    login module

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+import Register from '@/components/supaauth/register'
+import React from 'react'
+
+export default function page(){
+    return(
+        <div>
+            <Register/>
+        </div>
+    );
+}
