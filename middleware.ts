7b5d335ca9a22f8948ed5def45ca6df1268fabf2
Hector Carrillo <technik.und.spiel@gmail.com>
2024-11-21 21:08:35 -0600

    remove middleware

--- middleware.ts
+++ middleware.ts
@@ -16,34 +16,34 @@ export const config = {
 };
 
 export async function middleware(request: NextRequest) {
-  let response = NextResponse.next();
+//   let response = NextResponse.next();
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-        },
-        remove(name: string, options: CookieOptions) {
-          response.cookies.delete({
-            name,
-            ...options,
-          });
-        },
-      },
-    }
-  );
+//   const supabase = createServerClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//     {
+//       cookies: {
+//         get(name: string) {
+//           return request.cookies.get(name)?.value;
+//         },
+//         set(name: string, value: string, options: CookieOptions) {
+//           response.cookies.set({
+//             name,
+//             value,
+//             ...options,
+//           });
+//         },
+//         remove(name: string, options: CookieOptions) {
+//           response.cookies.delete({
+//             name,
+//             ...options,
+//           });
+//         },
+//       },
+//     }
+//   );
 
-  await supabase.auth.getSession();
+//   await supabase.auth.getSession();
 
-  return response;
+//   return response;
 }
